e18577b03ab8b475be4a4dd169b9828ea5c470bf
sawyerli <crazylh@gmail.com>
2016-04-01 14:56:38 +0800

    修改项目大体结构目录，引入sqlalchemy作为OR Mapping工具

--- app.py
+++ app.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def hello():
 	return "Hello World!"
 
-if __name__ == "__main__"
-	app.run()
+@app.route('/user/<name>')
+def user(name):
+	return '<h1>Hello, %s</h1>' % name
+
+if __name__ == "__main__":
+	app.run(debug=True)
